1dc921caebb8f3aa090331bde5f306f795063df3
NemanjaMandic <nemanja.mandic@folkmatic.com>
2019-03-15 22:55:01 +0100

    adding element references

--- src/SwipeableList/SwipeableListItem.jsx
+++ src/SwipeableList/SwipeableListItem.jsx
@@ -2,10 +2,21 @@ import "./SwipeableListItem.css";
 import React, { Component } from "react";
 
 export default class SwipeableListItem extends Component {
+
+    // DOM Refs
+    listElements;
+    wrapper;
+    background;
+
     render(){
         return (
             <>
-            
+                <div className="Wrapper" ref={div => (this.wrapper = div)}>
+                    <div className="Background">
+                        <span>Delete</span>
+                    </div>
+                    <div ref={div => (this.listElements = div)} className="ListItem">{ this.props.children }</div>
+                </div>
             </>
         );
     }
