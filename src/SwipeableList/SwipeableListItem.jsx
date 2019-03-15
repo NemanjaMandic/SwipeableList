a9ec9eba2963bd6e57cd32ee1535f2e6024adc7f
NemanjaMandic <nemanja.mandic@folkmatic.com>
2019-03-15 22:26:19 +0100

    ListItem added

---
+++ src/SwipeableList/SwipeableListItem.jsx
@@ -0,0 +1,12 @@
+import "./SwipeableListItem.css";
+import React, { Component } from "react";
+
+export default class SwipeableListItem extends Component {
+    render(){
+        return (
+            <>
+            
+            </>
+        );
+    }
+}
